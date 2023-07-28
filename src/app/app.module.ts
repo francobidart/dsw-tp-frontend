7ea22f36b420e072b084e8dbb363e101d24dd8f5
¨Franco <franbid@gmail.com>
2023-07-27 21:39:30 -0300

    Agregado home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import {RouterModule} from '@angular/router';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {path: '', component: HomeComponent}
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
