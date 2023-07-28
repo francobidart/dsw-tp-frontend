dd6cfe8bde548e19717e908bb75ea5ac2865ac2d
Gaspar356 <gasparandres2002@gmail.com>
2023-07-27 22:06:47 -0300

    aaasda

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductoComponent } from './producto/producto.component';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 import {RouterModule} from '@angular/router';
 @NgModule({
   declarations: [
     AppComponent,
+    ProductoComponent,
     HomeComponent,
     HeaderComponent
   ],
