8e13435027da6e64337ea60078c1b5697ddffa76
Gaspar356 <gasparandres2002@gmail.com>
2023-08-28 20:28:05 -0300

    down

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductoComponent } from './producto/producto.component';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
-import {RouterModule} from '@angular/router';
+import {Route, RouterModule} from '@angular/router';
+
+
+const routes: Route[] = [
+  { path: "", component: HomeComponent },
+  { path: "producto", component: ProductoComponent },
+  { path: "**", redirectTo: "" }
+];
+
 @NgModule({
   declarations: [
     AppComponent,
     ProductoComponent,
     HomeComponent,
-    HeaderComponent
+    HeaderComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
+    AppRoutingModule, RouterModule.forRoot(routes),FormsModule,
     RouterModule.forRoot([
       {path: '', component: HomeComponent}
+      
     ]),
   ],
   providers: [],
--- src/app/producto/producto.component.ts
+++ src/app/producto/producto.component.ts
@@ -1,15 +1,38 @@
 import { Component } from '@angular/core';
 
+interface Producto {
+  nombre: string;
+  descripcion: string;
+  precio: number;
+  imagen: string;
+  stock:number;
+}
+
 @Component({
   selector: 'app-producto',
   templateUrl: './producto.component.html',
   styleUrls: ['./producto.component.css']
 })
 export class ProductoComponent {
-  Id:number=0;
-  Precio:number=0;
-  Nombre:string='a';
-  Descripcion:string='a';
-  Stock:number=0;
+  productos: Producto[] = [];
+  nuevoProducto: Producto = {
+    nombre: '',
+    descripcion: '',
+    precio: 0,
+    imagen: '',
+    stock:0,
+  };
 
+  agregarProducto() {
+    if (this.nuevoProducto.nombre && this.nuevoProducto.descripcion && this.nuevoProducto.precio && this.nuevoProducto.imagen && this.nuevoProducto.stock) {
+      this.productos.push({ ...this.nuevoProducto });
+      this.nuevoProducto = {
+        nombre: '',
+        descripcion: '',
+        precio: 0,
+        imagen: '',
+        stock:0,
+      };
+    }
+  }
 }
